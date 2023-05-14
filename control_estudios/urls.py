48babde8290798c402307eb379764fa772e804f1
Sebastian Marambio <s.marambiosandoval@gmail.com>
2023-05-14 17:12:46 -0400

    se agrega formulario

--- control_estudios/urls.py
+++ control_estudios/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from sistema_coder.views import saludar, saludar_con_html
-from control_estudios.views import listar_estudiantes, listar_cursos
+from control_estudios.views import listar_estudiantes, listar_cursos, crear_cursos, crear_cursos2
 
 urlpatterns = [
     path("listarestu/", listar_estudiantes, name='estudiantes'),
     path("listarcursos/", listar_cursos, name='cursos'),
+    path("crear-cursos/", crear_cursos, name='crear_cursos_1'),
+    path("crear-cursos2/", crear_cursos2, name='crear_cursos_2'),
 ]
--- control_estudios/views.py
+++ control_estudios/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from control_estudios.models import Estudiante, Curso
+from control_estudios.forms import CursoFormulario
 
 
 def saludar_con_html(request):
-    contexto = {
-        "usuario":"Seba"
-    }
+    contexto = {"usuario": "Seba"}
     http_response = render(
         request=request,
         template_name="control_estudios/base.html",
@@ -14,9 +13,10 @@ def saludar_con_html(request):
     )
     return http_response
 
+
 def listar_estudiantes(request):
     contexto = {
-        "estudiantes":Estudiante.objects.all(),
+        "estudiantes": Estudiante.objects.all(),
     }
     http_response = render(
         request=request,
@@ -24,6 +24,7 @@ def listar_estudiantes(request):
         context=contexto,
     )
     return http_response
+
 
 def listar_cursos(request):
     contexto = {
@@ -36,3 +37,45 @@ def listar_cursos(request):
     )
     return http_response
 
+
+def crear_cursos(request):
+    if request.method == "POST":
+        data = request.POST
+        nombre = data["nombre"]
+        comision = data["comision"]
+        """ curso= Curso.objects.create(nombre=nombre, comision=comision) """
+        curso = Curso(nombre=nombre, comision=comision)
+        curso.save()
+        url_exitosa = reverse("cursos")
+        return redirect(url_exitosa)
+    else:
+        http_response = render(
+            request=request,
+            template_name="control_estudios/formulario_curso1.html",
+        )
+        return http_response
+
+
+def crear_cursos2(request):
+    if request.method == "POST":
+        formulario = CursoFormulario(request.POST)
+
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+            nombre = data["nombre"]
+            comision = data["comision"]
+            curso = Curso(nombre=nombre, comision=comision)
+            curso.save()
+            url_exitosa = reverse("cursos")
+            return redirect(url_exitosa)
+        else:
+            formulario = CursoFormulario(request.POST)
+
+    else:
+        formulario = CursoFormulario()
+        http_response = render(
+            request=request,
+            template_name="control_estudios/formulario_curso2.html",
+            context={'formulario': formulario}
+        )
+        return http_response
